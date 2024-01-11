cca42591118c047ba1b5ebee332bc96d9cc8a316
장정우 <58330276+jeongwoo903@users.noreply.github.com>
2024-01-12 01:08:34 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.cc
+++ 백준/Bronze/1546. 평균/평균.cc
@@ -1,32 +1,21 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-
 int main() {
-  int n, score;
-  float nf, max, result;
-  
-  vector<int> v;
-  vector<float> vf;
+  int n, max =0;
+  double score, sum, result =0;
 
   cin >> n;
-  int num = n;
 
-  while(num--) {
+  for(int i = 0; i < n; i++) {
     cin >> score;
-    v.push_back(score);
+    sum += score;
+    if(max < score)
+      max = score;
   }
 
-  sort(v.begin(), v.end(), greater<int>());
-  max = v.at(0);
-
-  for(int i = 0; i < n; i++)
-    nf += (v[i]/max) * 100;
-
-  result = nf/(float)n;
+  result = sum/max*100/n;
   cout << result;
 
   return 0;
