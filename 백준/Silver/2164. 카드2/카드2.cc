e6da438d5e5c7e3e51c31a81d9cbc5344cccf7b8
장정우 <58330276+jeongwoo903@users.noreply.github.com>
2024-01-20 16:09:20 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.cc
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -11,23 +11,16 @@ int main() {
   int n;
   cin >> n;
 
-  int temp;
-
   queue<int> q;
 
   for(int i = 1; i <= n; i++) {q.push(i);}
 
-  int i = 1;
-  while(q.size() != 1) { 
-    if(i % 2 == 1) {
+  while(q.size() > 1) { 
       q.pop();
-    } else {
-      temp = q.front();
+      q.push(q.front());
       q.pop();
-      q.push(temp);
-    }
-    i++;
   }
 
   cout << q.front();
+  return 0;
 }
