153d2717068c123550146e50c589f6deadcfabec
장정우 <58330276+jeongwoo903@users.noreply.github.com>
2024-01-25 18:15:43 +0900

    Update 수 정렬하기 2.cc

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -24,3 +24,81 @@ int main()
 
   return 0;
 }
+
+// merge sort로 구현한 것 - 시간초과 남.
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+ 
+// using namespace std;
+
+// void merge(vector<int> &v, int start, int pivot, int end);
+// int mergeSort(vector<int> &v, int start, int end);
+
+// int main() {
+//   ios::sync_with_stdio(false);
+//   cin.tie(NULL);
+//   cout.tie(NULL);
+
+//   int n;
+//   cin >> n;
+
+//   vector<int> v(n);
+
+//   for(int i = 0; i < n; i++) {
+//     cin >> v[i];
+//   }
+
+//   mergeSort(v, 0, n -1);
+
+//   for(int i = 0; i < n; i++) {
+//     cout << v[i] << "\n";
+//   }
+//   return 0;
+// }
+
+// void merge(vector<int> &v, int start, int pivot, int end) {
+//   int i = start;
+//   int j = pivot + 1;
+//   int k = start;
+
+//   vector<int> temp(v.size());
+
+//   while(i <= pivot && j <= end) {
+//     if(v[i] <= v[j]) {
+//       temp[k] = v[i];
+//       i++;
+//     } else {
+//       temp[k] = v[j];
+//       j++;
+//     }
+//     k++;
+//   }
+
+//   while(i <= pivot) { 
+//     temp[k] = v[i];
+//     i++;
+//     k++;
+//   }
+
+//   while(j <= end) { 
+//     temp[k] = v[j];
+//     j++;
+//     k++;
+//   }
+
+//   for(int idx = start; idx <= end; idx++) {
+//     v[idx] = temp[idx];
+//   }
+// }
+
+// int mergeSort(vector<int> &v, int start, int end) {
+//   if(start < end) {
+//     int pivot = (start + end) / 2;
+//     mergeSort(v, start, pivot);
+//     mergeSort(v, pivot + 1, end);
+//     merge(v, start, pivot, end);
+//   }
+
+//   return 0;
+// }
