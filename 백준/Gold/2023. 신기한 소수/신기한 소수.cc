001ed44d06a21751541063a0a0f0e13e295041c6
장정우 <58330276+jeongwoo903@users.noreply.github.com>
2024-01-26 23:41:36 +0900

    [Gold V] Title: 신기한 소수, Time: 704 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+ 
+using namespace std;
+
+void DFS(int num, int digit);
+bool isPrime(int k);
+static int N;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  
+  cin >> N;
+
+  DFS(2, 1);
+  DFS(3, 1);
+  DFS(5, 1);
+  DFS(7, 1);
+}
+
+void DFS(int num, int digit) {
+  if(digit == N) {
+    if(isPrime(num)) { cout << num << "\n"; }
+    return;
+  }
+
+  for(int i = 1; i < 10; i++) {
+    if(i % 2 == 0) { continue; }
+    if(isPrime(num*10 + i)) { DFS(num*10 + i, digit + 1); }
+  }
+}
+
+bool isPrime(int num) {
+  for(int i = 2; i <= num / 2; i++) {
+    if(num % i == 0) { return false; }
+  }
+  return true;
+}
