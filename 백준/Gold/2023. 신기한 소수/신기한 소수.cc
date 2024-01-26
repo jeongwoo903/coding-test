de57e32c38bf3dbbbb308856a9dccec606918706
장정우 <58330276+jeongwoo903@users.noreply.github.com>
2024-01-27 00:32:29 +0900

    [Gold V] Title: 신기한 소수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.cc
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.cc
@@ -32,7 +32,7 @@ void DFS(int num, int digit) {
 }
 
 bool isPrime(int num) {
-  for(int i = 2; i <= num / 2; i++) {
+  for(int i = 2; i*i <= num; i++) {
     if(num % i == 0) { return false; }
   }
   return true;
