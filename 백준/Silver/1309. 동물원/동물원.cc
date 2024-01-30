680c955cd64002f72ff8d05cb3b80277dce6586e
장정우 <58330276+jeongwoo903@users.noreply.github.com>
2024-01-31 03:02:27 +0900

    [Silver I] Title: 동물원, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+ 
+#define endl '\n'
+#define MAX 100001
+#define MOD 9901
+ 
+using namespace std;
+
+int DP(int num);
+static int arr[MAX];
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int n;
+  cin >> n;
+
+  cout << DP(n);
+  return 0;
+}
+
+int DP(int k) {
+    arr[0] = 1;
+    arr[1] = 3;
+    
+    for(int i = 2; i <= k; i++) {
+        arr[i] = (arr[i - 2] + arr[i - 1] * 2) % MOD;
+    }
+
+    return arr[k];
+}
