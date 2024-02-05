5493958abc43997f78f4209d42754abeb6e8d753
장정우 <58330276+jeongwoo903@users.noreply.github.com>
2024-02-05 14:26:11 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+ 
+#define endl '\n'
+ 
+using namespace std;
+
+int N, sum;
+vector<int> arr;
+
+int cnt = 0;
+void solve(int idx, int tmp) {
+
+	if (idx == N) return;
+	if (tmp + arr[idx] == sum) cnt++;
+
+	solve(idx + 1, tmp);
+	solve(idx + 1, tmp + arr[idx]);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+	cin >> N >> sum;
+
+	for (int i = 0; i < N; i++) {
+		int tmp;
+		cin >> tmp;
+		arr.push_back(tmp);
+	}
+
+	solve(0, 0);
+	cout << cnt;
+
+	return 0;
+}
